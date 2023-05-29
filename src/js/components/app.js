dc9435990f2434cbac9563d533a032714c82df80
GrigoryUK <g.usoltsev.worksrocket@gmail.com>
2023-05-29 22:45:43 +0500

    29.05.23

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,5 +1,9 @@
 import {headerJs} from "./header/header";
+import {feedbackForm} from "./header/feedbackForm";
+import smoothScroll from "./footer/smoothScroll";
 
 document.addEventListener('DOMContentLoaded', (event) => {
   headerJs()
+  feedbackForm()
+  smoothScroll()
 });
--- src/js/components/header/header.js
+++ src/js/components/header/header.js
@@ -2,9 +2,12 @@ import $ from 'jquery';
 import {isDesktop, isMobile, isTablet} from "../../functions/check-viewport";
 import {disableScrollCustom} from "../../functions/disable-scroll";
 import {enableScrollCustom} from "../../functions/enable-scroll";
+import disableScroll from 'disable-scroll';
 
 
 export function headerJs() {
+
+
   if (isDesktop()) {
 
     (function headerDropdownsDesk() {
@@ -100,5 +103,65 @@ export function headerJs() {
 
 
   }
+
+  (function toggleFeedback() {
+
+    const menu =  $('*[data-feedback-always]');
+
+
+    if (menu) {
+      const menuMain = $('*[data-menu-always]');
+      const buttonConnection = $('*[data-feedback-open-bid]');
+      const buttonCall = $('*[data-feedback-open-phone]');
+      const buttonClose =  $('*[data-feedback-close]');
+
+
+      buttonConnection.on('click', function () {
+       if (isDesktop()) {
+         disableScroll.on()
+       }
+       if (isMobile() || isTablet()) {
+         disableScrollCustom()
+       }
+        menu.fadeIn('fast', function () {
+          menu.find('.feedback__connection').addClass('active');
+        });
+
+
+      })
+
+      buttonCall.on('click', function () {
+        if (isDesktop()) {
+          disableScroll.on()
+        }
+        if (isMobile() || isTablet()) {
+          disableScrollCustom()
+        }
+        menu.fadeIn('fast', function () {
+          menu.find('.feedback__call').addClass('active');
+        });
+
+      })
+
+      buttonClose.on('click', function (e) {
+        e.preventDefault();
+        menu.find('.feedback__connection').removeClass('active');
+        menu.find('.feedback__call').removeClass('active');
+        menuMain.fadeOut('fast');
+        menu.fadeOut('slow');
+        if (isDesktop()) {
+          disableScroll.off()
+        }
+        if (isMobile() || isTablet()) {
+          enableScrollCustom()
+        }
+
+      })
+    }
+
+
+
+
+  })();
 
 }
